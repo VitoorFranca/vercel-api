cf7b6e60d41381b497bc319161075c6ef44905ab
Caio Silva <caiodassilva@gmail.com>
2024-02-06 11:53:35 -0300

    mais melhorias

--- src/services/SlackService.ts
+++ src/services/SlackService.ts
@@ -1,55 +1,40 @@
-import axios, { Axios } from "axios";
+import axios, { AxiosInstance } from "axios";
 import { buttonPayload, modalPayload } from "../utils/slack";
 
 export class SlackService {
-    client: Axios;
+  private client: AxiosInstance;
 
   constructor() {
-    this.client = new axios.Axios({
-        baseURL:  "https://slack.com/api/",
-        headers: {
-            'Content-Type': 'application/json',
+    this.client = axios.create({
+      baseURL: "https://slack.com/api/",
+      headers: {
+        'Content-Type': 'application/json',
             Authorization: `Bearer ${process.env.SLACK_TOKEN}`
     }});
   }
 
   async sendMessage(options: any): Promise<object> {
-    try {        
-        const response = await this.client.post("chat.postMessage", {
-            ...buttonPayload,
-                ...options
-        },
-        {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${process.env.SLACK_TOKEN}`
-            },
-        }
-        )
-        console.log(response)
-        return response;
-    } catch (error : any) {      
+    try {
+      const response = await this.client.post("chat.postMessage", {
+        ...buttonPayload,
+        ...options
+      })
+      console.log(response)
+      return response.data;
+    } catch (error: any) {
       throw new Error('API Error: ' + error.message);
     }
   }
   async openModal(triggerId: string): Promise<object> {
-      try {
-          const response = await this.client.post("views.open", {
-              ...modalPayload,
-              trigger_id: triggerId
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${process.env.SLACK_TOKEN}`
-                },
-            }
-            
-            )
-            console.log(response)
-      
-      return response;
-    } catch (error : any) {
+    try {
+      const response = await this.client.post("views.open", {
+        ...modalPayload,
+        trigger_id: triggerId
+      })
+      console.log(response)
+
+      return response.data;
+    } catch (error: any) {
       throw new Error('API Error: ' + error.message);
     }
   }
