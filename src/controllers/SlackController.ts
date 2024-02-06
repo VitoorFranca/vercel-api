39fed476c832282e299c48d365f6b76ea4cc475a
Caio Silva <caiodassilva@gmail.com>
2024-02-06 10:51:15 -0300

    adicionando logs

--- src/controllers/SlackController.ts
+++ src/controllers/SlackController.ts
@@ -11,20 +11,24 @@ export class SlackController {
   async openModal(req: Request, res: Response): Promise<void> {
     try {
       const payload = JSON.parse(req.body.payload)
+      console.log("payload open modal: " + payload)
 
       const result = await this.slackService.openModal(payload.trigger_id);
+      console.log("request: " + result);
       res.status(200).json(result);
     } catch (error) {
-      res.status(200).json({ message: 'Error converting currencies' });
+      console.log('Error open modal: ' + error )
+      res.status(400).json({ message: 'Error open modal: ' + error });
     }
   }
   async sendMessage(req: Request, res: Response): Promise<void> {
     try {
       const result = await this.slackService.sendMessage(req.body);
+      console.log("request: " + result);
       res.status(200).json(result);
     } catch (error) {
-      // console.log(error)
-      res.status(200).json({ message: 'Error converting currencies' });
+      console.log('Error open modal: ' + error )
+      res.status(400).json({ message: 'Error send message: ' + error });
     }
   }
 }
